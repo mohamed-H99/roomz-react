53644ea941a910d0773e323ae698f31032b064d5
ahmdbhgat4 <ahmdbhgat4@gmail.com>
2021-10-28 00:33:54 +0200

    fixing auth issues

--- src/storeProvider.js
+++ src/storeProvider.js
@@ -181,7 +181,7 @@ export default function StoreProvider({ children }) {
     })();
 
     return () => {};
-  }, [state.submittingForm]);
+  }, [state.signupForm.uname, state.submittingForm]);
 
   return (
     <DispatchContext.Provider value={dispatch}>
